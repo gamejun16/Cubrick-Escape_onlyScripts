04bc621f51bf738d5441ea3732841ce69b6770c0
Minjun Jin <gamejun16@daum.net>
2021-01-01 16:40:33 +0900

    add backend scripts

---
+++ Scripts/HomeScene/RankingManager.cs
@@ -0,0 +1,245 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using BackEnd;
+
+using UnityEngine.UI;
+using System.Text;
+using LitJson;
+
+public class RankingManager : MonoBehaviour
+{
+    public GameObject enrollTab, rankingTab;
+
+    [Header("EnrollTab")]
+    public Text[] levelTxts;
+    public Text[] stepTxts;
+    public Text[] timeTxts;
+    public Image[] upperFrame, lowerFrame;
+
+    [Header("RankingTab")]
+    public Transform[] levelRankings;
+    
+
+    #region enroll tab
+
+    public void OpenEnrollTab()
+    {
+        enrollTab.SetActive(true);
+
+        MobileDeviceSupporter.instance.AddToUICloser(CloseEnrollTab);
+
+        // 스코어 정보 받아오기
+        // 각 레벨의 스테이지가 모두 클리어 되어야 랭킹에 등록할 수 있다.
+
+        // Unlock된 최고 레벨
+        // 2라면 1레벨은 전부 클리어 된 것.
+        int highLevel = PlayerPrefs.GetInt("level");
+
+        StringBuilder sb = new StringBuilder();
+
+        for (int lv=1;lv<highLevel; lv++)
+        {
+            // 테두리 밝게
+            upperFrame[lv - 1].color = Color.yellow;
+            lowerFrame[lv - 1].color = Color.yellow;
+
+            // 글자 밝게
+            levelTxts[lv - 1].color = Color.white;
+            stepTxts[lv - 1].color = Color.white;
+            timeTxts[lv - 1].color = Color.white;
+
+            // 토탈 step, time 가져온 후 출력
+            int totalStep = DataContainer.instance.GetGameRecordStagesTotalStep(lv);
+            float totalTime = DataContainer.instance.GetGameRecordStagesTotalTime(lv);
+            
+            sb.Append(totalStep.ToString()).Append(" step");
+            stepTxts[lv - 1].text = sb.ToString();
+            sb.Clear();
+
+            sb.AppendFormat("{0:F2}", totalTime.ToString()).Append(" sec");
+            timeTxts[lv - 1].text = sb.ToString();
+            sb.Clear();
+        }
+
+        // 사운드
+        UIAudioManager.instance.AudioPlay(1);
+    }
+
+    public void CloseEnrollTab()
+    {
+        enrollTab.SetActive(false);
+
+        MobileDeviceSupporter.instance.RemoveFromUICloser(CloseEnrollTab);
+
+        // 사운드
+        UIAudioManager.instance.AudioPlay(1);
+    }
+
+    /// <summary>
+    /// 로컬 데이터를 서버와 동기화하는 함수
+    /// 동기화한 후 Enroll tab을 close하고 Ranking tab을 open한다
+    /// </summary>
+    public void EnrollDataToServer()
+    {
+        BackendReturnObject bro;
+                
+        bro = Backend.GameSchemaInfo.Get("timeRanking", new Where(), 1);
+        JsonData rows;
+
+        // 데이터가 없다면 init 데이터 생성 및 진행
+        if (!bro.IsSuccess())
+        {
+            Backend.GameSchemaInfo.Insert("timeRanking");
+            bro = Backend.GameSchemaInfo.Get("timeRanking", new Where(), 1);
+        }
+        
+        rows = bro.Rows();
+        
+        string rankTableIndate = rows[0]["inDate"]["S"].ToString(); // its different with user-indate
+        List<int> myScores = new List<int>();
+
+        // 스테이지를 다 클리어 한 레벨에 대해서만 갱신 진행.
+        for (int i=1;i<PlayerPrefs.GetInt("level"); i++)
+        {   
+            // total time 자료형은 float.
+            // UpdateRTRankTable 에서는 int 자료형만 Update 되므로 float->int 형변환 과정을 거침
+            float totalTime = DataContainer.instance.GetGameRecordStagesTotalTime(i);
+            int totalTimeFloatToInt = (int)Mathf.Round(totalTime * 100);
+            myScores.Add(totalTimeFloatToInt);
+
+            Backend.GameSchemaInfo.UpdateRTRankTable("timeRanking", "lv" + i, totalTimeFloatToInt, rankTableIndate);
+        }
+        
+        // load rank data
+        // Level1RankData uuid, etc.
+        string[] uuid =
+        {
+            "8f752f60-09e3-11eb-bd5a-2d114656860e", // lv1 
+            "23c21ad0-0a10-11eb-ad47-3f256a7ec4da", // lv2
+            "2e28ea80-0a10-11eb-bd5a-2d114656860e", // lv3
+            "34981a30-0a10-11eb-ad47-3f256a7ec4da", // lv4
+            "3b0af360-0a10-11eb-bd5a-2d114656860e" // lv5
+        };
+        
+        //for (int i = 0; i < uuid.Length; i++)
+        for (int i = 0; i < 5; i++)
+        {
+            bro = Backend.RTRank.GetRTRankByUuid(uuid[i]);
+
+            JsonData otherRows = null, myRows = null;
+
+            if (bro.IsSuccess())
+            {
+                LogMonitor.instance.AddLog($"Get Rank success");
+
+                otherRows = bro.Rows();
+
+                LogMonitor.instance.AddLog($"[lv{i+1}]. [{otherRows.Count}]명 랭킹 데이터 존재");
+            }
+            else
+            {
+                LogMonitor.instance.AddLog($"Get lv{i+1} Rank fail");
+            }
+
+
+            if(i >= myScores.Count)
+            {
+                InitRankingTabDatas(i, otherRows, myRows);
+                continue;
+            }
+            
+            bro = Backend.RTRank.GetRTRankByScore(uuid[i], myScores[i]);
+
+            if (bro.IsSuccess())
+            {
+                LogMonitor.instance.AddLog($"Get My Rank Success, rows count : {bro.Rows().Count}");
+
+                myRows = bro.Rows();
+            }
+            else
+            {
+                LogMonitor.instance.AddLog($"Get lv{i + 1} My Rank fail : {bro}");
+            }
+            
+
+            InitRankingTabDatas(i, otherRows, myRows);
+
+        }
+
+        CloseEnrollTab();
+        OpenRankingTab();
+    }
+
+    /// <summary>
+    /// EnrollTab에서 읽어들인 데이터들을 RankingTab에 표시하는 함수
+    /// </summary>
+    void InitRankingTabDatas(int idx, JsonData otherRows, JsonData myRows)
+    {
+        StringBuilder sb = new StringBuilder();
+
+        Transform otherScore = levelRankings[idx].GetChild(1);
+        Transform myScore = levelRankings[idx].GetChild(2);
+
+        // otherScore
+        List<Transform> scores = new List<Transform>();
+
+        // 1~10위까지 표시.
+        // 기록 표시 Text 추출 및 기록 갱신
+        for (int j = 0; j < otherRows.Count; j++)
+        {
+            Text txt = otherScore.GetChild(j).GetChild(1).GetChild(2).GetComponent<Text>();
+            var v = otherRows[j]["score"]["N"];
+            float time = int.Parse(v.ToString()) * 0.01f;
+
+            sb.Clear();
+            sb.Append(time.ToString()).Append("<size=30>sec</size>");
+            txt.text = sb.ToString();
+        }
+
+
+        // myScore
+        Text myRankTxt = myScore.GetChild(0).GetChild(2).GetComponent<Text>();
+        Text myScoreTxt = myScore.GetChild(1).GetChild(2).GetComponent<Text>();
+
+
+        // my ranking 데이터가 없는 경우.
+        if (myRows == null) return;
+
+        sb.Clear();
+        sb.Append(myRows[0]["rank"]["N"].ToString());
+        myRankTxt.text = sb.ToString();
+
+        sb.Clear();        
+        sb.Append((int.Parse(myRows[0]["score"]["N"].ToString()) * 0.01f).ToString()).Append("<size=30>sec</size>");
+        myScoreTxt.text = sb.ToString();
+
+    }
+    
+    #endregion
+    
+    #region ranking tab
+
+    public void OpenRankingTab()
+    {
+        rankingTab.SetActive(true);
+
+        MobileDeviceSupporter.instance.AddToUICloser(CloseRankingTab);
+
+        // 사운드
+        UIAudioManager.instance.AudioPlay(1);
+    }
+    
+    public void CloseRankingTab()
+    {
+        rankingTab.SetActive(false);
+
+        MobileDeviceSupporter.instance.RemoveFromUICloser(CloseRankingTab);
+
+        // 사운드
+        UIAudioManager.instance.AudioPlay(1);
+    }
+
+    #endregion
+
+}
---
+++ Scripts/System/BackendManager.cs
@@ -0,0 +1,484 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+using GooglePlayGames;
+using GooglePlayGames.BasicApi;
+using UnityEngine.SocialPlatforms;
+
+using BackEnd;
+using LitJson;
+
+/**
+ * 
+ * 뒤끝과 연결되어 게임 내에서 백앤드와 관련된 처리들을 총괄하는 스크립트
+ * 
+ * ex) 로그인, 랭킹 조회, ...
+ * 
+ * */
+
+public class BackendManager : MonoBehaviour
+{
+    static public BackendManager instance;
+
+    [Header("TitleScene")]
+    public TitleSceneManager tcm;
+    
+
+    // 재접속 최대 시도 카운트
+    // 초과시 진행 불가, 재접속 유도
+    int backendRetryCount = 5;
+
+    #region Log variables
+
+    float logTimer = 0f;
+    IEnumerator logCoroutine;
+
+    #endregion
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // singleton
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+            Destroy(instance.gameObject);
+
+#if UNITY_EDITOR
+
+        // 준비 완료
+        LogMonitor.instance.AddLog("Playform : UNITY_EDITOR");
+        tcm.ReadyToStart();
+
+#elif UNITY_ANDROID
+
+        LogMonitor.instance.AddLog("Playform : UNITY_ANDROID");
+
+        GPGSInit();
+        BackendInit();
+#endif
+
+    }
+
+    void GPGSInit()
+    {
+        PlayGamesClientConfiguration config = new PlayGamesClientConfiguration
+    .Builder()
+    .RequestServerAuthCode(false)
+    .RequestIdToken()
+    .Build();
+
+        PlayGamesPlatform.InitializeInstance(config);
+
+        PlayGamesPlatform.DebugLogEnabled = true;
+        //GPGS 시작.
+        PlayGamesPlatform.Activate();
+    }
+
+    void BackendInit()
+    {
+        // backend init
+        Backend.Initialize(() =>
+        {
+            // 초기화 성공한 경우 실행
+            if (Backend.IsInitialized)
+            {
+                // example
+                // 버전 체크 -> 업데이트
+                
+                LogMonitor.instance.AddLog("Game server is initialized now... (1/3)");
+                
+                GPGSLogin();
+            }
+
+            // 초기화 실패한 경우 실행
+            else
+            {
+                if (FailToConnectBackend("Backend Init is fail !!"))
+                {
+                    Invoke("BackendInit", 1f);
+                }
+            }
+        });
+
+    }
+    
+    public void GPGSLogin()
+    {
+        LogMonitor.instance.AddLog("GPGSLogin called()");
+
+        // 이미 로그인 된 경우
+        if (Social.localUser.authenticated == true)
+        {
+            LogMonitor.instance.AddLog("gpgs login info is exist (2/3)");
+            Backend.BMember.AuthorizeFederation(GetTokens(), FederationType.Google, "gpgs", (callback) =>
+            {
+                // gpgs 로그인 성공 -> 뒤끝 로그인 성공
+                if (callback.IsSuccess())
+                {
+                    LogMonitor.instance.AddLog("뒤끝 login is success (3/3)");
+
+
+                    // 준비 완료
+                    tcm.ReadyToStart();
+                }
+
+                // gpgs 로그인 성공 -> 뒤끝 로그인 실패
+                else
+                {
+                    if (FailToConnectBackend("뒤끝 login is fail"
+                    + $"\n[ERROR] Error : {callback}"
+                    ))
+                    {
+                        // 재접속 시도
+                        Invoke("GPGSLogin", 1);
+                    }
+                }
+            });
+        }
+        else
+        {
+            Social.localUser.Authenticate((bool success) =>
+            {
+                if (success)
+                {
+                    LogMonitor.instance.AddLog("gpgs login is success (2/3)");
+
+                    // 로그인 성공 -> 뒤끝 서버에 획득한 구글 토큰으로 가입요청
+                    Backend.BMember.AuthorizeFederation(GetTokens(), FederationType.Google, "gpgs", (callback)=>
+                    {
+                        // gpgs 로그인 성공 -> 뒤끝 로그인 성공
+                        if (callback.IsSuccess())
+                        {
+                            LogMonitor.instance.AddLog("뒤끝 login is success (3/3)");
+                            
+                            // 준비 완료
+                            tcm.ReadyToStart();
+                        }
+                        // gpgs 로그인 성공 -> 뒤끝 로그인 실패
+                        else
+                        {
+                            if (FailToConnectBackend("뒤끝 login is fail"
+                                + $"\n[ERROR] Error : {callback}"
+                                ))
+                            {
+                                // 재접속 시도
+                                Invoke("GPGSLogin", 1);
+                            }
+                        }
+
+                    });
+                }
+                else
+                {
+                    // gpgs 로그인 실패
+                    if (FailToConnectBackend("gpgs login is fail"))
+                    {
+                        // 재접속 시도
+                        Invoke("GPGSLogin", 1);
+                    }
+                }
+            });
+        }
+    }
+    
+    public void GPGSLogout()
+    {
+        ((PlayGamesPlatform)Social.Active).SignOut();
+        LogMonitor.instance.AddLog("## GPGS Logout ##");
+    }
+
+    /// <summary>
+    /// 구글 토큰 받아오기
+    /// </summary>
+    /// <returns>토큰을 문자열로 반환</returns>
+    public string GetTokens()
+    {
+        if (PlayGamesPlatform.Instance.localUser.authenticated)
+        {
+            // 유저 토큰 받기 첫번째 방법
+            string _IDtoken = PlayGamesPlatform.Instance.GetIdToken();
+            // 두번째 방법
+            // string _IDtoken = ((PlayGamesLocalUser)Social.localUser).GetIdToken();
+            
+
+            return _IDtoken;
+
+        }
+        else
+        {
+            Debug.Log("접속되어있지 않습니다. PlayGamesPlatform.Instance.localUser.authenticated :  fail");
+            
+            return null;
+        }
+    }
+
+    /// <summary>
+    /// 네트워크 접속 실패시 호출
+    /// </summary>
+    /// <returns>true: 재접속 진행, false: 재접속 불가능, 강제종료 유도</returns>
+    bool FailToConnectBackend(string log)
+    {
+        LogMonitor.instance.AddLog(log);
+
+        // 계정 삭제 버튼 활성화
+        tcm.DeleteBtnOpen();
+
+        backendRetryCount -= 1;
+        if (backendRetryCount > 0)
+        {
+            LogMonitor.instance.AddLog($"재연결 시도중입니다.. 남은 횟수 : {backendRetryCount}");
+            return true;
+        }
+        else
+        {
+            LogMonitor.instance.AddLog($"연결 실패. 재접속하세요.");
+            return false;
+        }
+    }
+
+    /// <summary>
+    /// 서버로부터 스테이지 정보를 Load
+    /// # 1. 서버에 정보가 있는 경우 -> inDate 추출, 저장 및 사용
+    /// </summary>
+    public void LoadGameRecordDataInit()
+    {
+        LogMonitor.instance.AddLog("LoadGameRecordData() is called ...");
+        
+        Backend.GameInfo.GetPrivateContents("gameRecord", (callback) =>
+        {
+            if (callback.IsSuccess())
+            {
+                LoadGameRecordData(callback);
+            }
+            else
+            {
+                LogMonitor.instance.AddLog("Fail to load stage data");
+            }
+        });
+    }
+
+    /// <summary>
+    /// InitStageInfo() 비동기 접속 성공시 호출
+    /// </summary>
+    void LoadGameRecordData(BackendReturnObject bro)
+    {
+        var rows = bro.Rows();
+
+        // # 1.
+        if (rows.Count > 0)
+        {
+            LogMonitor.instance.AddLog("There are data");
+
+            string jsonData = rows[0]["jsonData"]["S"].ToString();
+            DataContainer.instance.SetGameRecordFromJson(jsonData);
+            
+            LogMonitor.instance.AddLog("data load done");
+        }
+
+        // # 2.
+        else
+        {
+            LogMonitor.instance.AddLog("There are no data");
+
+        }
+    }
+
+
+    /// <summary>
+    /// GameRecord 정보를 뒤끝 db로 업로드
+    /// </summary>
+    public void SaveGameRecordData()
+    {
+        LogMonitor.instance.AddLog("SaveGameRecordData() is called ...");
+
+        Param param = new Param();
+        
+        string jsonData = DataContainer.instance.GetGameRecordToJson();
+        
+        param.Add("jsonData", jsonData);
+
+        //// lv1 ~ lv5 column 채우기.
+        //for (int i = 1; i < PlayerPrefs.GetInt("level"); i++)
+        //{
+        //    // 각 레벨 column에 레벨별 기록한 시간을 저장
+        //    float totalTime = DataContainer.instance.GetGameRecordStagesTotalTime(i);
+        //    param.Add("lv" + i, (int)Mathf.Round(totalTime * 100));
+        //}
+        //// 아직 레벨스코어가 없다면(다 깨지 않아서) infinity로 초기화
+        //for (int i = PlayerPrefs.GetInt("level"); i <= 5; i++)
+        //{
+        //    param.Add("lv" + (i).ToString(), 123456789);
+        //}
+
+        BackendReturnObject bro = Backend.GameInfo.GetPrivateContents("gameRecord");
+        
+        var rows = bro.Rows();
+        
+        // 데이터가 있는 경우. -> Update
+        if (rows.Count > 0)
+        {
+            string indate = rows[0]["inDate"]["S"].ToString();
+            
+            Backend.GameInfo.Update("gameRecord", indate, param, (callback) =>
+            {
+                if (callback.IsSuccess())
+                    LogMonitor.instance.AddLog("SaveGameRecordData() -> Update is success");
+                else
+                {
+                    LogMonitor.instance.AddLog("SaveGameRecordData() -> Update is fail");
+                    LogMonitor.instance.AddLog($"error : {callback}");
+                }
+            });
+        }
+
+        // 데이터가 없는 경우. -> Insert
+        else
+        {
+            Backend.GameInfo.Insert("gameRecord", param, (callback) => {
+                if (callback.IsSuccess())
+                {
+                    LogMonitor.instance.AddLog("SaveGameRecordData() -> Insert is success");
+                    LogMonitor.instance.AddLog($"indate : {callback.GetInDate()}");
+                }
+                else
+                {
+                    LogMonitor.instance.AddLog("SaveGameRecordData() -> Insert is fail");
+                    LogMonitor.instance.AddLog($"error : {callback}");
+                }
+            });
+           
+        }
+    }
+
+
+    /// <summary>
+    /// 로컬에 저장된 게스트 계정 정보를 삭제
+    /// </summary>
+    public void DeleteGuestUserInfo()
+    {
+        Backend.BMember.DeleteGuestInfo();
+        LogMonitor.instance.AddLog("## 계정 정보가 삭제되었습니다 !! ##");
+        Application.Quit();
+    }
+
+#region unuse
+
+    /// <summary>
+    /// 뒤끝 비동기 게스트 로그인 함수
+    /// </summary>
+    //void AGuestLogin()
+    //{
+    //    //Debug.Log("-------------A GuestLogin-------------");
+
+    //    // login
+    //    Backend.BMember.GuestLogin((callback) =>
+    //    {
+
+    //        //Debug.Log(callback);
+
+    //        // 로그인 성공
+    //        if (callback.IsSuccess())
+    //        {
+    //            LogMonitor.instance.AddLog("Login is success. Wait for load stage info... (2/2)");
+
+    //            // 준비 완료
+    //            tcm.ReadyToStart();
+
+    //        }
+    //        else
+    //        {
+    //            if (FailToConnectBackend("Backend Init is fail !!"
+    //                + $"\n[ERROR] Error : {callback}"
+    //                + $"\n[ERROR] Error Code : {callback.GetErrorCode()}"
+    //                + $"\n[ERROR] Message : {callback.GetMessage()}"
+    //                ))
+    //            {
+    //                // 재접속 시도
+    //                Invoke("AGuestLogin", 1);
+    //            }
+
+    //            //print($"[DEV] 로그인 실패");
+    //            //LogMonitor.instance.AddLog("[DEV] Login is fail !!");
+    //            //LogMonitor.instance.AddLog($"[ERROR] Error : {callback}");
+    //            //LogMonitor.instance.AddLog($"[ERROR] Error Code : {callback.GetErrorCode()}");
+    //            //LogMonitor.instance.AddLog($"[ERROR] Message : {callback.GetMessage()}");
+    //            //LogMonitor.instance.AddLog($"***** Try Login again *****");
+    //        }
+
+    //    });
+    //}
+
+
+    /// <summary>
+    /// 
+    ///         tmp
+    ///         구글 해시 키 얻기 위한 함수
+    ///         
+    /// </summary>
+    //public void GetGoogleHash()
+    //{
+    //    inputField.text = Backend.Utils.GetGoogleHash();
+    //}
+
+    //[ContextMenu("Data save")]
+    //void StageSave()
+    //{
+    //    if(inDate != null)
+    //    {
+
+    //    }
+    //    else if (inDate == null)
+    //    {
+
+    //        int curLevel = Random.Range(0, 10);
+    //        int curStage = Random.Range(100, 110);
+
+    //        Param param = new Param();
+
+    //        param.Add("level", curLevel);
+    //        param.Add("stage", curStage);
+
+    //        BackendReturnObject bro = Backend.GameInfo.Insert("stage", param);
+
+    //        if (bro.IsSuccess())
+    //        {
+    //            print($"[DEV] 데이터 저장 성공");
+    //        }
+    //        else
+    //        {
+    //            print($"[DEV] 데이터 저장 실패");
+    //        }
+    //    }
+    //}
+
+
+    //[ContextMenu("Stage load")]
+    //void StageLoad()
+    //{
+    //    //print($"[DEV] level : {Backend.GameInfo.GetPrivateContents("stage", "level")}");
+    //    BackendReturnObject bro = Backend.GameInfo.GetPrivateContents("stage");
+
+
+
+    //    var rows = bro.Rows();
+
+    //    JsonData data = rows[0];
+    //    if (data.Keys.Contains("level"))
+    //    {
+    //        //var output = data["level"]["N"];
+    //        print($"[DEV] level : {data["level"]["N"]}");
+    //        print($"[DEV] stage : {data["stage"]["N"]}");
+
+    //        print($"[DEV] indate : {data["inDate"]["S"]}");
+    //        inDate = data["inDate"]["S"].ToString();
+    //    }
+    //}
+
+#endregion
+}
